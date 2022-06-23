4f39eba10c10117a6bd07c1c0b67f8aafc85d04d
jnsougata <dwipjana293@gmail.com>
2022-06-23 08:47:27 +0530

    changes

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ from aiotube import Channel
 app = fastapi.FastAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": Channel('GYROOO').info}
+@app.get("/ch/{id}")
+async def info(id: str = None):
+    if id:
+        channel = Channel(id)
+        payload = {
+            'latest': {},
+            'stream': {},
+            'info': channel.info
+        }
+        upload = channel.recent_uploaded
+        if upload:
+            latest_url = upload.url
+            latest_id = latest_url.split('=')[-1]
+            payload['latest'] = {'id': latest_id, 'url': latest_url}
+
+        stream = channel.livestream
+        if stream:
+            stream_url = stream.url
+            stream_id = stream_url.split('=')[-1]
+            payload['stream'] = {'id': stream_id, 'url': stream_url}
+
+        return payload
