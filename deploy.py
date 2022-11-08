65582aae542de8b065990824f5edca6e0b3ad0a2
jnsougata <jnsougata@gmail.com>
2022-11-08 17:08:07 +0530

    fuck the backup

--- deploy.py
+++ deploy.py
@@ -3,37 +3,40 @@ import deta_cli
 
 
 MICROS = [
-    'shard-zero',
-    'shard-one',
-    'shard-two',
-    'shard-three',
-    'shard-four',
-    'shard-five',
-    'shard-six',
-    'shard-seven',
-    'shard-eight',
-    'shard-nine',
+    'shard-backup',
+    #'shard-zero',
+    #'shard-one',
+    #'shard-two',
+    #'shard-three',
+    #'shard-four',
+    #'shard-five',
+    #'shard-six',
+    #'shard-seven',
+    #'shard-eight',
+    #'shard-nine',
 ]
 
 def main():
     deta_access_key = sys.argv[1]
     if not deta_access_key:
         raise sys.exit('No Deta access key provided')
+    
+    try:
+        client = deta_cli.DetaClient(deta_access_key)
+    except Exception as e:
+        raise sys.exit(f'Error: Invalid Deta access key...')
+    else:
+        for name in MICROS:
+            try:
+                micro = client.get_micro(name)
+                micro.remove_deps('aiotube', 'fastapi')
+                micro.add_deps('aiotube', 'fastapi')
+                micro.deploy(scripts=['main.py', 'requirements.txt'])
+                sys.stdout.write(f'[SUCCESS] {name} deployed...')
+            except Exception as e:
+                sys.stdout.write(f'[ERROR] {name} failed to deploy...')
+                sys.stdout.write(e)
 
-    client = deta_cli.DetaClient(deta_access_key)
-
-    for name in MICROS:
-        try:
-            micro = client.get_micro(name)
-            micro.remove_deps('aiotube', 'fastapi')
-            micro.add_deps('aiotube', 'fastapi')
-            micro.deploy(scripts=['main.py', 'requirements.txt'])
-            sys.stdout.write(f'[SUCCESS] {name} deployed...')
-        except Exception as e:
-            sys.stdout.write(f'[ERROR] {name} failed to deploy...')
-            sys.stdout.write(e)
-
-
+    
 if __name__ == '__main__':
     main()
-    
